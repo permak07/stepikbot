4c39bbfd749629dcd0d75eb75ea06494859a5757
Nikita Permyakov <permakovnikita70@gmail.com>
2025-11-11 11:04:34 +0800

    some_file.py это файл с логированием

--- python1/some_file.py
+++ python1/some_file.py
@@ -1,3 +1,38 @@
-print("This is file from github repositore")
+#======================логи=====================
+import logging
 
-print("first commit")
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug('Это лог уровня DEBUG')
+logging.info('Это лог уровня INFO')
+logging.warning('Это лог уровня WARNING')
+logging.error('Это лог уровня ERROR')
+logging.critical('Это лог уровня CRITICAL')
+
+#======================формат отображения логов=====================
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[{asctime}] #{levelname:8} {filename}:'
+           '{lineno} - {name} - {message}',
+    style='{'
+)
+
+logger = logging.getLogger(__name__)
+
+logger.debug('Лог уровня DEBUG')
+
+#======================форматтеры=====================
+import logging
+
+format_1 = '#%(levelname)-8s [%(asctime)s] - %(filename)s:'\
+           '%(lineno)d - %(name)s - %(message)s'
+format_2 = '[{asctime}] #{levelname:8} {filename}:'\
+           '{lineno} - {name} - {message}'
+
+formatter_1 = logging.Formatter(fmt=format_1)
+formatter_2 = logging.Formatter(
+    fmt=format_2,
+    style='{'
+)
